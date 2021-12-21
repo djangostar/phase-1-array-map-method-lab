e9c26e7bb33d0c5c9e0b3b6ecd63df2104a7b813
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-21 12:49:03 -0800

    commit tree 95d9d3e847723bda4b92af4d48ff6adbb134ff81

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorials) => {
+  return tutorials.map(line => {
+    const lineSplit = line.split(' ')
+    return lineSplit.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  })
 }
+  //return map through tutorials and split the lines into individual words
+  //iterate over words and at index 0, uppercase word
+  //use slice and join to join the words back together
+ 
